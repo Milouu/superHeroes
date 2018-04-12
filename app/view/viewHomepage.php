d83f391d9479da5543fa751fd91db9f2895c0e98
Arthur-GH <arthur.girault.5@gmail.com>
2018-04-12 13:52:56 +0200

    :lipstick: Homepage front

--- app/view/viewHomepage.php
+++ app/view/viewHomepage.php
@@ -1,18 +1,51 @@
 <?php $this->title = "Super Heroes" ?>
-
+<div class="row">
+    </div>
+    <div class="row">
+        <div class="col-3">
+            <img class="homepageImg" src="./assets/images/cpt-america.svg">
+        </div>
+        <div class="col-3 offset-10">
+            <img class="homepageImg" src="./assets/images/ironman.svg">
+        </div>
 <div class="container">
     <div class="row homepageTitleContainer">
         <h1 class="col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-sm-8 offset-sm-2 col-xs-8 offset-xs-2 homepageTitle">Hero Battlefield Tournament</h1>
     </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 gameResume">Hero Battlefield Tournament is a card-fighting system featuring superheroes from all walks of life. Between strategy and chance, it's up to you to compose a team of superheroes able to defeat your opponent and bring you to victory! General rules : The championship happens in one week, with an action to be done every day. Your team is made up of 5 superheroes, each with different abilities. During the fighting, these heroes clash and bring you (or not) to victory!</div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 gameResume"> Compose a team of superheroes able to defeat your opponent and bring you victory!</div>        
+    <div class="row homepageStepOne align-items-center m-5">
+        <img class="col-lg-6 col-md-6 offset-2 stepsImg" src="./assets/images/StepOne.svg">
+        <div class="col-lg-4 col-md5 col-sm-5 -2 col-xs-5">
+            <h1 class="homepageSteps">STEP ONE</h1>
+            <p class="homepageStepsDescription"> Create a league with your friends </p>
+        </div>
+    </div>
+    <div class="row homepageStepTwo align-items-center m-5">
+        <div class="col-lg-4 col-md4 col-sm-4 col-xs-4 offset-2">
+            <h1 class="homepageSteps">STEP TWO</h1>
+            <p class="homepageStepsDescription"> Choose your superheroes by bidding with your friends to get the best players. 1000 RP are to spend!</p>
+        </div>
+        <img class="col-lg-6 col-md-6 stepsImg" src="./assets/images/StepTwo.svg">
+        
+    </div>
+    <div class="row homepageStepThree align-items-center m-5">
+        <img class="col-lg-6 col-md-6 offset-2 stepsImg" src="./assets/images/StepThree.svg">
+        <div class="col-lg-4 col-md5 col-sm-5 -2 col-xs-5">
+            <h1 class="homepageSteps">STEP THREE</h1>
+            <p class="homepageStepsDescription"> Order your cards and defeat your opponent ! </p>
+        </div>
+    </div>
+    <div class="row homepageStepFour align-items-center m-5">
+    <div class="col-lg-4 col-md4 col-sm-4 col-xs-4 offset-2">
+            <h1 class="homepageSteps">STEP FOUR</h1>
+            <p class="homepageStepsDescription">Discover the championship results and laugh at your poor friends ...</p>
+        </div>
+        <img class="col-lg-6 col-md-6  stepsImg" src="./assets/images/StepFour.svg">
+        
     </div>
     <div class="homepageButtonContainer">
         <div class="row justify-content-center">
             <button class="registerButton"><a href="index.php?action=signup">I want to register !</a></button>
-        </div>
-        <div class="row justify-content-center mt-3">
-            <button class="readTheRulesButton"><a href="#">I want to read the rules first</a></button>
         </div>
         <div class="row">
             <a href="index.php?action=signin" class="mt-2 mb-3 col-lg-4 offset-lg-4 col-md-4 offset-md-4 col-sm-4 offset-sm-4 col-xs-4 offset-xs-4 signInLink">I already have an account !</a>
---
+++ app/view/viewRules.php
@@ -0,0 +1,14 @@
+<?php $this->title = "Rules" ?>
+
+<div class="container">
+<div class="rulesContainer>">
+<h2 class="Title">New league</h2>
+    <p class="rulesText">
+        Hero Battlefield Tournament is a card fighting system featuring superheroes from all walks of life. Between strategy and chance, it's up to you to compose a team of superheroes able to defeat your opponent and bring you to victory!
+
+    General rules : 
+    The championship happens in one week, with an action to be done every day. Your team is made up of 5 superheroes, each with different abilities. During the fighting, these heroes clash and bring you (or not) to victory!
+    </p>
+
+</div> 
+</div>   
