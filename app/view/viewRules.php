b8d404aa7e565ccce7d0a55125a44e80780eb37b
Mathildefournier <mathilde.fournier@hetic.net>
2018-04-12 12:01:45 +0200

    :lipstick: add rules pages

--- app/view/viewRules.php
+++ app/view/viewRules.php
@@ -1,1 +1,101 @@
-<?php $this->title = "Recruit Superheroes" ?>
+<?php $this->title = "Rules" ?>
+
+<div class="container rulesContainer pb-4">
+  <div class="row">
+    <div class="rulesTittle col-lg-12"><h2>Rules of the game</h2></div>
+  </div>
+  <div class="container justify-content-center">
+    <div class="row">
+    <div class="container col-lg-12">
+        <div class="rulesText container col-lg-10">Hero Battlefield Tournament is a card-fighting system featuring superheroes from all walks of life. Between strategy and chance, it's up to you to compose a team of superheroes able to defeat your opponent and bring you to victory!
+            <br/><br/>
+            <h4>General rules :</h4><br/>
+            The championship happens in one week, with an action to be done every day. Your team is made up of 5 superheroes, each with different abilities. During the fighting, these heroes clash and bring you (or not) to victory!
+            <br/><br/>
+            <h4>Organization of the week:</h4><br/>
+            Monday: first betting round to choose the superheroes of your team. Choose your favorite heroes from the list and add next to the amount you would be willing to spend to get it back!
+            Each hero has a minimum price set according to his characteristics.
+            Attention, your opponents do the same, it will have to be strategic ...
+            <br/>
+            Tuesday: At the end of the first round, your bids have been accepted ... Or no. Purchased heroes are transferred to your team, and you now need to do a second round of bidding to recover missing members and have a full team.
+            <br/>
+            Wednesday: Your team is now complete. The championship will start, you can see your future opponents and order your cards! The match is as follows: Players must place their heroes in a certain order the day before and draw 2 bonus / penalty cards.
+            They can decide to play them on opposing heroes by seeing the opposing composition. Players do not know which field they play on.
+            <br/>
+            Thursday: Qualifying match after the players use their bonus / malus.
+            <br/>
+            Friday: Quarter-finals, bonus / penalty use.
+            <br/>
+            Saturday: Semi-final.
+            <br/>
+            Sunday: Final
+            <br/><br/>
+            <h4>Course of a match:</h4><br/>
+            Rules of the championship: the leagues must be made up of 8 players.
+            There will be 28 games in all.
+            <br/>
+            The matches are played in one against one. You can order your heroes (1, 2, 3, 4, 5) your superheroes who will be in combat against the superheroes of your opponent (placed in the same order). The one with the highest ability score between the two wins, after calculating the penalty / bonus then the field. In the end, it is the player with the most wins who wins the match.
+            <br/><br/>
+            <h4>Rewards:</h4><br/>
+            Players start with 3500 reputation points to buy their heroes. Everyone gets them at the end of the tournament but:
+            The winner of the championship recovers 700 more.
+            Players who reached the final get 500.
+            The third gets 300.
+            All others get 100.
+            <br/><br/>
+
+            <h4>Characters:</h4><br/>
+            The superheroes of all the universes are present in this game: from DC Comics to Marvel via Stargate or Harry Potter, build your dream team to fight your friends!
+            Each character has 6 skills: intelligence, strength, speed, endurance, power and combat.
+            An average of these skills is made and gives the total strength of your hero: it is this number that will make the difference in games.
+            <br/>
+            Heroes are classified into strength categories: 0 - 40/40 - 60/60 - 80/80 - 100.
+            Some characters are sensitive to their environment or some bonus cards, use them in the right way to ensure a place in the final!
+            <br/><br/>
+            <h4>Land:</h4><br/>
+            There are 8 different terrains, some of which affect the skills of the heroes:
+            Asgard: affects the strength ability (-50%).
+            Atlantis: affects endurance ability (-50%)
+            United States: neutral ground, heroes
+            France: neutral ground
+            Gotham City: affects combat ability (-50%)
+            Genasha Island: affects the ability to power (-50%)
+            Krypton: affects speed and halves category 4 character skills (average> 80)
+            Wakanda: affects intelligence ability (-50%)
+            Coral City: All heroes in category 1 (average <40) have their average doubled.
+            <br/><br/>
+
+            <h4>Bonus:</h4><br/>
+            To spice up the game, a list of 13 bonuses / malus is available. During the strategy implementation phase, you can choose to award two bonuses per game, which you will not be able to pick up during the rest of the tournament.
+            You can add one or two bonuses on the same character, or assign a penalty on one of your opponent's cards.
+            <br/>
+            List of bonuses:<br/>
+            <br/>
+            Bonus:<br/>
+            Batmobile -> double the speed of a character<br/>
+            Jarvis -> double intelligence<br/>
+            Deadpool -> double endurance<br/>
+            You have recovered Mjöllnir -> doubles the fight<br/>
+            Luke Cage -> doubles the power<br/>
+            Hawkeye -> hide your character from the opponent's eyes<br/>
+            Groot -> increases by x1,5% the average<br/>
+            Hulk -> double your strength<br/>
+            Venom -> removes bonuses from an opposing hero
+            <br/><br/>
+            Malus:<br/>
+            Kryptonite -> halves the force<br/>
+            Vibranium was stolen -> halving stamina<br/>
+            Thanos just arrived -> x1.5% decrease in average<br/>
+            Loki -> halves intelligence<br/>
+            Joker -> halves the fight<br/>
+            Mister Freeze -> halves the speed
+            </div>
+    </div>
+    </div>
+    <img src="assets/images/batman.svg" alt="Batman" class="batman"/>
+    <div class="row mt-4">
+      <button class="rulesButton col-lg-2 offset-lg-2"><a href="index.php?action=leagues&option=create">Start a game</a></button>
+    </div>
+    <img src="assets/images/rocket.svg" alt="Rocket Racoon" class="rocket"/>
+  </div>
+</div>
