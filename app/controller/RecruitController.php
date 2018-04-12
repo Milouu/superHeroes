f2f344b52d42b635156dc6cc39b0d974c43aaa85
Rémi de Juvigny <remidejuvigny@gmail.com>
2018-04-12 10:57:10 +0200

    :lock: Handle recruitment errors

--- app/controller/RecruitController.php
+++ app/controller/RecruitController.php
@@ -25,22 +25,51 @@ class RecruitController
   // Check if user recruitments are valid
   public function tryRecruit()
   {
-    if (isset($_POST['selections'])) {
-      $selections = $_POST['selections'];
-      // Call function to save to DB
-      foreach ($selections as $selectedHero) {
+    $errorMessage = '';
+
+    if (!isset($_POST['selections']) || count($_POST['selections']) != 5)
+    {
+      $errorMessage = 'Please select 5 heroes';
+    }
+
+    $selections = $_POST['selections'];
+    $totalPrice = 0;
+    foreach ($selections as $selectedHero)
+    {
+      $detailedHero = $this->hero->getHero($selectedHero);
+      $totalPrice += $detailedHero[0]->average * 10;
+    }
+
+    if ($totalPrice > 3500)
+    {
+      $errorMessage = 'Your heroes cost ' . $totalPrice . ' RP, but you only have 3500 RP';
+    }
+
+    if (strlen($errorMessage) > 0) 
+    {
+      // Get heroes from database
+      $heroes = $this->hero->getHeroes(isset($_POST['sorting']) ? $_POST['sorting'] : 'average');
+
+      // Display error message
+      $_POST['error'] = $errorMessage;
+      $view = new View('Recruit');
+      $view->generate(array('heroes' => $heroes));
+    }
+    else
+    {
+      // Save purchases to database
+      foreach ($selections as $selectedHero)
+      {
         $detailedHero = $this->hero->getHero($selectedHero);
         $purchaseData = array(
           'hero_id' => $selectedHero,
           'league_id' => $_GET['league_id'],
           'user_id' => $_SESSION['user_id'],
-          'price' => $detailedHero[0]->average
+          'price' => $detailedHero[0]->average * 10
         );
+
         $this->hero->buyHero($purchaseData);
       }
-    } else {
-      $view = new View('Recruit');
-      $view->generate(array('heroes' => $heroes));
     }
   }
 }
