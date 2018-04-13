3bf29bf4be455a3517ea5c4388ee639f0914cad4
Milouu <milan.roustan@gmail.com>
2018-04-13 04:08:16 +0200

    :lipstick: Better UX next league day

--- app/view/viewDashboard.php
+++ app/view/viewDashboard.php
@@ -175,9 +175,11 @@
     </ul>
   </div>
 <?php endforeach; ?>
-<div class="col-lg-4 pt-5">
-  <a href="index.php?action=dashboard&league_id=11&option=nextDay" class="nextDay registerButton pl-2 pr-2" title="Next day">Next league day</a>
 </div>
+<div class="row">
+  <div class="col-lg-12 pt-5 mb-4">
+    <a href="index.php?action=dashboard&league_id=11&option=nextDay" class="nextDay registerButton pl-2 pr-2" title="Next day">Next league day</a>
+  </div>
 </div>
 <?php endif; ?>
 
